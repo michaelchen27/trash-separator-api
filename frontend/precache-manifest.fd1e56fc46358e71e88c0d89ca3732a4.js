3c5651ea2ce7e6cc67292aa7312a38198aeca22f
ryukinlika <58260433+ryukinlika@users.noreply.github.com>
2021-12-16 02:32:57 +0800

    update some frontend

---
+++ frontend/precache-manifest.fd1e56fc46358e71e88c0d89ca3732a4.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a1e3b6efc40e5c77f56844a563d8185",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3b3ce9c97ca9a2bf7fb",
+    "url": "/static/css/3.4a0e35e1.chunk.css"
+  },
+  {
+    "revision": "242409fef1186463ca6a",
+    "url": "/static/css/main.8211599d.chunk.css"
+  },
+  {
+    "revision": "a3b3ce9c97ca9a2bf7fb",
+    "url": "/static/js/3.d3bf4a51.chunk.js"
+  },
+  {
+    "revision": "9fc001496a73c09130b1b48b3a7e1ec2",
+    "url": "/static/js/3.d3bf4a51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3327f84b8a9622ec9f6",
+    "url": "/static/js/4.9abebc4a.chunk.js"
+  },
+  {
+    "revision": "701cc973ef4910f0caa6",
+    "url": "/static/js/5.766cf5df.chunk.js"
+  },
+  {
+    "revision": "4eeff70c89569cc50d564670b0645ce5",
+    "url": "/static/js/5.766cf5df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ce83a72dd8bea4d7254",
+    "url": "/static/js/6.35511900.chunk.js"
+  },
+  {
+    "revision": "49c2feb24fa8a0f7d99deba74f4e90d7",
+    "url": "/static/js/6.35511900.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b48fb54b35e247600db5",
+    "url": "/static/js/7.f474b2cf.chunk.js"
+  },
+  {
+    "revision": "242409fef1186463ca6a",
+    "url": "/static/js/main.080e81a2.chunk.js"
+  },
+  {
+    "revision": "86ffe0294855b12252da",
+    "url": "/static/js/runtime-main.fc75322d.js"
+  },
+  {
+    "revision": "cfb49707a4c5e2a291c5",
+    "url": "/static/js/xlsx.496b8a91.chunk.js"
+  }
+]);
